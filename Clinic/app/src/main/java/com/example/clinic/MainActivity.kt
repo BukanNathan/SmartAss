0af47d1202af37f7871f4ecce2a364377d51fd55
ferdinanlarifhiento <74222390+ferdinanlarifhiento@users.noreply.github.com>
2021-01-30 18:49:26 +0700

    test

--- Clinic/app/src/main/java/com/example/clinic/MainActivity.kt
+++ Clinic/app/src/main/java/com/example/clinic/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.clinic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.maps.MapView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val inten1 = findViewById<Button>(R.id.map)
+        inten1.setOnClickListener{
+            val map = Intent(this, MapsFragment::class.java)
+            startActivity(map)
+        }
     }
+
 }
