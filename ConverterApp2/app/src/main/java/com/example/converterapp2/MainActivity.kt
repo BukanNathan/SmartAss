73efc7e8b4c9fe97220b3d76e3df64486f78e0c9
BukanNathan <76737883+BukanNathan@users.noreply.github.com>
2021-03-11 21:41:31 +0700

    App-Converter-2
    with onSaveInstance

---
+++ ConverterApp2/app/src/main/java/com/example/converterapp2/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.converterapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val EXTRA_STATUS = "EXTRA_STATUS"
+private const val EXTRA_STATUS2 = "EXTRA_STATUS2"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            textView6.text = editTextNumber.text
+            textView7.text = editTextNumber2.text
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(EXTRA_STATUS, textView6.text.toString())
+        outState.putString(EXTRA_STATUS2, textView7.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textView6.text = savedInstanceState.getString(EXTRA_STATUS)
+        textView7.text = savedInstanceState.getString(EXTRA_STATUS2)
+    }
+}
