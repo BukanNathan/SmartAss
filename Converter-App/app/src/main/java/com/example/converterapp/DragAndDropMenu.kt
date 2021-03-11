17efbcacdcb22df4381bc655923204978260936f
BukanNathan <76737883+BukanNathan@users.noreply.github.com>
2021-03-11 16:07:07 +0700

    Converter-App

---
+++ Converter-App/app/src/main/java/com/example/converterapp/DragAndDropMenu.kt
@@ -0,0 +1,11 @@
+package com.example.converterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DragAndDropMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drag_and_drop_menu)
+    }
+}
