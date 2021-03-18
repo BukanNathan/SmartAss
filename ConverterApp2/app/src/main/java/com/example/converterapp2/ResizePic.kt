5dafed06de724ead92dac690e8d66085f254a155
BukanNathan <76737883+BukanNathan@users.noreply.github.com>
2021-03-18 21:59:23 +0700

    penambahan filter dengan thread

--- ConverterApp2/app/src/main/java/com/example/converterapp2/ResizePic.kt
+++ ConverterApp2/app/src/main/java/com/example/converterapp2/ResizePic.kt
@@ -3,11 +3,19 @@ package com.example.converterapp2
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_resize.*
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
 
 private const val EXTRA_STATUS = "EXTRA_STATUS"
 private const val EXTRA_STATUS2 = "EXTRA_STATUS2"
@@ -18,6 +26,56 @@ class ResizePic : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resize)
         actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val addImg = findViewById<Button>(R.id.filter1);
+        val addImg2 = findViewById<Button>(R.id.filter2);
+        val addImg3 = findViewById<Button>(R.id.filter3);
+        val addImg4 = findViewById<Button>(R.id.filter4);
+        val viewImg = findViewById<ImageView>(R.id.viewFilter);
+        var strURL = "https://i.ibb.co/VLrzf9V/pngwing-com-1.png"
+        var strURL2 = "https://i.ibb.co/X5ZkYdk/pngwing-com.png"
+        var strURL3 = "https://i.ibb.co/G3x0H82/instragram-love-filters.png"
+        var strURL4 = "https://i.ibb.co/Zf7tpVJ/forest-border.png"
+
+        addImg.setOnClickListener {
+            Thread(Runnable{
+                val bitmap = processBitMap(strURL)
+                viewImg.post{
+                    println("Menambahkan gambar");
+                    viewImg.setImageBitmap(bitmap);
+                }
+            }).start()
+        }
+
+        addImg2.setOnClickListener {
+            Thread(Runnable{
+                val bitmap = processBitMap(strURL2)
+                viewImg.post{
+                    println("Menambahkan gambar");
+                    viewImg.setImageBitmap(bitmap);
+                }
+            }).start()
+        }
+
+        addImg3.setOnClickListener {
+            Thread(Runnable{
+                val bitmap = processBitMap(strURL3)
+                viewImg.post{
+                    println("Menambahkan gambar");
+                    viewImg.setImageBitmap(bitmap);
+                }
+            }).start()
+        }
+
+        addImg4.setOnClickListener {
+            Thread(Runnable{
+                val bitmap = processBitMap(strURL4)
+                viewImg.post{
+                    println("Menambahkan gambar");
+                    viewImg.setImageBitmap(bitmap);
+                }
+            }).start()
+        }
 
         button.setOnClickListener {
             textView6.text = editTextNumber.text
@@ -32,6 +90,21 @@ class ResizePic : AppCompatActivity() {
             else {
                 Toast.makeText(this, "Unable to Open Camera", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private fun processBitMap(url : String) : Bitmap?{
+        return try {
+            val url = URL(url)
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.doInput = true
+            connection.connect()
+            val input:InputStream = connection.inputStream
+            val myBitmap = BitmapFactory.decodeStream(input)
+            myBitmap
+        }catch (e:IOException){
+            e.printStackTrace()
+            null
         }
     }
 
