9dd760e4ea0a02fe42bc2f058e4489c5c0bd1a44
BukanNathan <76737883+BukanNathan@users.noreply.github.com>
2021-06-11 00:14:22 +0700

    alarm manager widget

--- ConverterApp2/app/src/main/java/com/example/converterapp2/MainActivity.kt
+++ ConverterApp2/app/src/main/java/com/example/converterapp2/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.converterapp2
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_file_list.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,5 +39,16 @@ class MainActivity : AppCompatActivity() {
             val intentInternalSave = Intent(this, InternalSave::class.java)
             startActivity(intentInternalSave)
         }
+
+/** Widget */
+//        var alarmIntent = Intent(this, MyMessage::class.java).let {
+//            it.action = MyMessage.ACTION_AUTO_UPDATE
+//            PendingIntent.getBroadcast(this, 101, it, PendingIntent.FLAG_UPDATE_CURRENT)
+//        }
+//        var cal = Calendar.getInstance()
+//        cal.add(Calendar.MINUTE, 1)
+//
+//        var alarmManager = this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+//        alarmManager.setRepeating(AlarmManager.RTC, cal.timeInMillis, 60000L, alarmIntent)
     }
 }
