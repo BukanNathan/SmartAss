aa9e3e328720c8336672a98ac9f5326e40c25124
BukanNathan <76737883+BukanNathan@users.noreply.github.com>
2021-06-17 22:15:29 +0700

    Firebase Message

---
+++ ConverterApp2/app/src/main/java/com/example/converterapp2/MyFirebaseMessagingService.kt
@@ -0,0 +1,42 @@
+package com.example.converterapp2
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+        if(p0.notification!=null){
+            showNotify(p0.notification!!.title, p0.notification!!.body)
+        }
+    }
+
+    private fun showNotify(title:String?, message: String?){
+        var notifManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val notifChannel = NotificationChannel("notif1", "Notification", NotificationManager.IMPORTANCE_DEFAULT)
+            notifChannel.description = "FCM Channel 1"
+            notifChannel.enableLights(true)
+            notifChannel.lightColor = Color.RED
+            notifManager.createNotificationChannel(notifChannel)
+        }
+
+        var notifyNotif = NotificationCompat.Builder(this, "notif").apply {
+            setDefaults(Notification.DEFAULT_ALL)
+            setWhen(System.currentTimeMillis())
+            setSmallIcon(R.mipmap.ic_launcher)
+            setContentTitle(title)
+            setContentText(message)
+            setContentInfo("information")
+        }
+        notifManager.notify(1,notifyNotif.build())
+    }
+}
